fd2e6bbe7a71e8470847f6918827f75c8c2fbade
Aeron Laffere <ajlaffere@gmail.com>
2024-11-23 21:53:01 +0000

    fix: just use 20vh to calculate top margin instead of calc

--- src/app/NHSSpendingCalculator.tsx
+++ src/app/NHSSpendingCalculator.tsx
@@ -115,8 +115,8 @@ export default function NHSSpendingCalculator() {
 
 	return (
 		<>
-			<div className="min-h-screen pb-24">
-				<div className="max-w-[1024px] mx-auto px-4 mt-[calc(50vh-300px)]">
+			<div className="min-h-screen pb-24 pt-6">
+				<div className="max-w-[1024px] mx-auto px-4 mt-[20vh]">
 					<div className="flex flex-col items-center lg:items-end justify-end gap-3 mb-4 bg-gradient-to-r from-neutral-100 to-neutral-50 shadow-sm border p-4 rounded-lg">
 						<span className="text-muted-foreground w-fit text-sm order-2 lg:order-1 text-center">
 							Join the conversation about Britain's future on the{" "}
